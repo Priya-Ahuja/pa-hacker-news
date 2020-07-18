ad1966834f1b6b7199d3fb97215b45f6f07ba675
Priya Ahuja <priahuja@oradev.oraclecorp.com>
2020-07-18 16:12:46 +0530

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { NewsChartComponent } from './news-chart/news-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsFeedComponent,
+    NewsChartComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
